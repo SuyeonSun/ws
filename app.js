35d2a7ef08d9910c9f68837ea68a6a101143445c
SuyeonSun <suninisy@gmail.com>
2024-07-11 17:09:05 +0900

    채팅앱 완성

--- app.js
+++ app.js
@@ -13,22 +13,23 @@ app.listen(8000, () => {
 })
 // 서버 실행 > node app.js
 
-// 웹소켓 서버 생성
 const wss = new WebSocketServer({port: 8001})
 
-// 웹소켓 서버 연결 이벤트 바인드
-// wss: 웹소켓 서버, ws: 연결된 클라이언트
-wss.on('connection', (ws, request) => {
-    // 데이터 수신 이벤트 바인드
-    ws.on("message", data => {
-        console.log(`Received from user: ${data}`)
-        ws.send(`Received from user: ${data}`)
+wss.on("connection", (ws, request) => {
+    ws.on("close", () => {
+        wss.clients.forEach((client) => {
+            client.send(`유저 한명이 떠났습니다. 현재 유저 ${wss.clients.size} 명`);
+        })
     })
 
-    ws.send(`Hello, ${request.socket.remoteAddress}`) // 연결 직후 클라이언트로 데이터 전송
-    /*
-    * 이렇듯, 웹소켓은 클라이언트의 요청 없이도 능동적으로 먼저 클라이언트에게 데이터를 전송할 수 있는 전이중통신 (Full-Duplex Communication) 을 지원한다.
-    * 따라서 HTTP 와 다르게 요청과 응답을 따로 구분하지 않는다.
-    */
+    ws.on("message", data => {
+        wss.clients.forEach(client => {
+            client.send(data.toString())
+        })
+    })
 
+    // 클라이언트 브로드캐스트: 새로운 클라이언트가 접속하면 모든 클라이언트에게 알림
+    wss.clients.forEach(client => {
+        client.send(`새로운 유저 접속, 현재 유저 ${wss.clients.size} 명`)
+    })
 })
